fb90e856ad78454074cd9a5ee602738bd91d57e6
matejsuty <sutymatej@gmail.com>
2023-03-01 19:20:57 +0100

    Basic structure of react app and buttons with inputs components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import React from "react";
+// import UilReact from "@iconscout/react-unicons/icons/uil-react";
+import TopButtons from "./components/TopButtons";
+import Inputs from "./components/Inputs";
+
+function App() {
+  return (
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <TopButtons />
+      <Inputs />
+    </div>
+  );
+}
+
+export default App;
